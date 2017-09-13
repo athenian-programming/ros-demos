fb708f8bf3025b99626d5aefdb53a72a74217744
Paul Ambrose <pambrose@mac.com>
2017-09-13 13:18:03 -0700

    Update xbox remapper

--- topic_remapper/scripts/xbox_remapper.py
+++ topic_remapper/scripts/xbox_remapper.py
@@ -5,33 +5,30 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
 
-def vels(target_linear_vel, target_angular_vel):
-    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel, target_angular_vel)
+def vels(dir, target_linear_vel, target_angular_vel):
+    return "%s:\tlinear vel %s\t angular vel %s" % (dir, target_linear_vel, target_angular_vel)
 
 def callback(msg):
     global pub, target_linear_vel, target_angular_vel, control_linear_vel, control_angular_vel
-    if msg.axes[6] != 0.0 or msg.axes[7] != 0.0:
-        print("Remapping {0} {1}".format(msg.axes[6], msg.axes[7]))
 
     if msg.axes[7] == 1.0:
         target_linear_vel = target_linear_vel + 0.01
-        print vels(target_linear_vel, target_angular_vel)
+        print vels("Forward", target_linear_vel, target_angular_vel)
     elif msg.axes[7] == -1.0:
         target_linear_vel = target_linear_vel - 0.01
-        print vels(target_linear_vel, target_angular_vel)
+        print vels("Backward", target_linear_vel, target_angular_vel)
     elif msg.axes[6] == 1.0:
         target_angular_vel = target_angular_vel + 0.1
-        print vels(target_linear_vel, target_angular_vel)
+        print vels("Left", target_linear_vel, target_angular_vel)
     elif msg.axes[6] == -1.0:
         target_angular_vel = target_angular_vel - 0.1
-        print vels(target_linear_vel, target_angular_vel)
+        print vels("Right", target_linear_vel, target_angular_vel)
     elif msg.buttons[0] == 1 or msg.buttons[1] == 1 or msg.buttons[2] == 1 or msg.buttons[3] == 1:
-        print("Stopping")
         target_linear_vel = 0
         control_linear_vel = 0
         target_angular_vel = 0
         control_angular_vel = 0
-        print vels(target_linear_vel, target_angular_vel)
+        print vels("Stop", target_linear_vel, target_angular_vel)
 
     if target_linear_vel > control_linear_vel:
         control_linear_vel = min(target_linear_vel, control_linear_vel + (0.01 / 4.0))
