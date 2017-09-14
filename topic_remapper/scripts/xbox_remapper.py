0c8712143e432781ee9e79fa025e0a353a079223
Paul Ambrose <pambrose@mac.com>
2017-09-14 12:18:26 -0700

    Misc update

--- topic_remapper/scripts/xbox_remapper.py
+++ topic_remapper/scripts/xbox_remapper.py
@@ -13,43 +13,44 @@ def vels(dir, target_linear_vel, target_ang_vel):
     return "%s:\tlinear vel %s\t angular vel %s" % (dir, target_linear_vel, target_ang_vel)
 
 
-def bound(min_val, max_val, val):
-    if val < min_val:
-        return min_val
-    elif val > max_val:
-        return max_val
+def bound(val):
+    if val < -1.0:
+        return -1.0
+    elif val > 1.0:
+        return 1.0
     else:
         return val
+
 
 def callback(msg):
     global pub, target_linear_vel, target_ang_vel, control_linear_vel, control_ang_vel
 
     # Map button cross
     if msg.axes[7] == 1.0:
-        target_linear_vel = target_linear_vel + LINEAR_ADJ
+        target_linear_vel = bound(target_linear_vel + LINEAR_ADJ)
         print vels("Forward ", target_linear_vel, target_ang_vel)
     elif msg.axes[7] == -1.0:
-        target_linear_vel = target_linear_vel - LINEAR_ADJ
+        target_linear_vel = bound(target_linear_vel - LINEAR_ADJ)
         print vels("Backward", target_linear_vel, target_ang_vel)
     elif msg.axes[6] == 1.0:
-        target_ang_vel = target_ang_vel + ANG_ADJ
+        target_ang_vel = bound(target_ang_vel + ANG_ADJ)
         print vels("Left    ", target_linear_vel, target_ang_vel)
     elif msg.axes[6] == -1.0:
-        target_ang_vel = target_ang_vel - ANG_ADJ
+        target_ang_vel = bound(target_ang_vel - ANG_ADJ)
         print vels("Right   ", target_linear_vel, target_ang_vel)
 
     # Map left joystick
     elif msg.axes[1] > 0.0:
-        target_linear_vel = target_linear_vel + (LINEAR_ADJ * msg.axes[1])
+        target_linear_vel = bound(target_linear_vel + (LINEAR_ADJ * msg.axes[1]))
         print vels("Forward   ", target_linear_vel, target_ang_vel)
     elif msg.axes[1] < 0.0:
-        target_linear_vel = target_linear_vel - (LINEAR_ADJ * -msg.axes[1])
+        target_linear_vel = bound(target_linear_vel - (LINEAR_ADJ * -msg.axes[1]))
         print vels("Backward   ", target_linear_vel, target_ang_vel)
     elif msg.axes[0] > 0.0:
-        target_ang_vel = target_ang_vel + (ANG_ADJ * msg.axes[0])
+        target_ang_vel = bound(target_ang_vel + (ANG_ADJ * msg.axes[0]))
         print vels("Left    ", target_linear_vel, target_ang_vel)
     elif msg.axes[0] < 0.0:
-        target_ang_vel = target_ang_vel - (ANG_ADJ * -msg.axes[0])
+        target_ang_vel = bound(target_ang_vel - (ANG_ADJ * -msg.axes[0]))
         print vels("Right   ", target_linear_vel, target_ang_vel)
 
     # Map all right button presses to stop
@@ -85,12 +86,12 @@ def callback(msg):
         control_ang_vel = target_ang_vel
 
     twist = Twist()
-    twist.linear.x = bound(-1.0, 1.0, control_linear_vel)
+    twist.linear.x = bound(control_linear_vel)
     twist.linear.y = 0
     twist.linear.z = 0
     twist.angular.x = 0
     twist.angular.y = 0
-    twist.angular.z = bound(-1.0, 1.0, control_ang_vel)
+    twist.angular.z = bound(control_ang_vel)
     pub.publish(twist)
 
 
