2b44f2ffd2bdee26009280c6ba66d94b1313a5b1
Paul Ambrose <pambrose@mac.com>
2017-09-19 12:17:31 -0700

    Misc update

--- imu_teleop/scripts/imu_teleop.py
+++ imu_teleop/scripts/imu_teleop.py
@@ -24,18 +24,23 @@ def linear_callback(msg):
         target_linear_vel = val
         control_linear_vel = min(1.0,
                                  control_linear_vel + (
-                                     0.01 * val)) if target_linear_vel > control_linear_vel else target_linear_vel
-        print(vels("Forward   ", target_linear_vel, target_ang_vel))
+                                     0.001 * val)) if target_linear_vel > control_linear_vel else target_linear_vel
+        print(vels("Forward   ", control_linear_vel, target_ang_vel))
     elif val <= -0.1:
         target_linear_vel = val
         control_linear_vel = max(-1.0,
                                  control_linear_vel - (
-                                     0.01 * -val)) if target_linear_vel < control_linear_vel else target_linear_vel
-        print(vels("Backward   ", target_linear_vel, target_ang_vel))
+                                     0.001 * -val)) if target_linear_vel < control_linear_vel else target_linear_vel
+        print(vels("Backward   ", control_linear_vel, target_ang_vel))
     else:
+        if control_linear_vel >= 0.1:
+            control_linear_vel = control_linear_vel - .01
+        elif control_linear_vel <= 0.1:
+            control_linear_vel = control_linear_vel + .01
+        else
+            control_linear_vel = 0
         target_linear_vel = 0
-        control_linear_vel = 0
-        print(vels("Linear Stop", target_linear_vel, target_ang_vel))
+        print(vels("Linear Stop", control_linear_vel, target_ang_vel))
 
 
 def ang_callback(msg):
@@ -90,6 +95,6 @@ if __name__ == '__main__':
         twist.angular.x = 0
         twist.angular.y = 0
         twist.angular.z = control_ang_vel
-        print("Writing: %s" % control_linear_vel)
+        # print("Writing: %s" % control_linear_vel)
         pub.publish(twist)
         rate.sleep()
