9bd1692a353180fc8f2f6fb3a223503a63c3bcd7
Paul Ambrose <pambrose@mac.com>
2017-09-19 20:56:27 -0700

    Misc update

--- imu_teleop/scripts/imu_teleop.py
+++ imu_teleop/scripts/imu_teleop.py
@@ -15,89 +15,27 @@ def vels(dir, target, control):
 
 
 def linear_callback(msg):
-    global linear_raw, linear_adj, target_linear, curr_linear
+    global linear_raw, linear_adj, curr_linear
 
-    # linear_raw = max(-1.0, min(1.0, int((msg.pose.pose.orientation.y * 10)) / 5.0))
     linear_raw = max(-1.0, min(1.0, ((msg.pose.pose.orientation.y / 2.0) * 10) / 5.0))
 
     if linear_adj is None:
         linear_adj = linear_raw
 
     # Normalize to initial value
-    target_linear = linear_raw - linear_adj
-
-    if target_linear >= ZERO_TOP:
-        if target_linear > curr_linear:
-            curr_linear = curr_linear + (INC * MULT)
-        elif target_linear < curr_linear:
-            curr_linear = curr_linear - (INC * MULT)
-        else:
-            curr_linear = target_linear
-        curr_linear = min(1.0, curr_linear)
-        # print(vels("Forward   ", curr_linear, target_linear))
-    elif target_linear <= ZERO_BOTTOM:
-        if target_linear > curr_linear:
-            curr_linear = curr_linear + (INC * MULT)
-        elif target_linear < curr_linear:
-            curr_linear = curr_linear - (INC * MULT)
-        else:
-            curr_linear = target_linear
-        curr_linear = max(-1.0, curr_linear)
-        # print(vels("Backward   ", curr_linear, target_linear))
-    else:
-        target_linear = 0
-        if curr_linear >= ZERO_TOP:
-            curr_linear = curr_linear - INC
-        elif curr_linear <= ZERO_BOTTOM:
-            curr_linear = curr_linear + INC
-        else:
-            curr_linear = 0
-            # print(vels("Linear Stop", curr_linear, target_linear))
-
-    curr_linear = linear_raw - linear_adj
+    curr_linear = int((linear_raw - linear_adj) * 100) / 100.0
 
 
 def ang_callback(msg):
-    global ang_raw, ang_adj, target_ang, curr_ang
+    global ang_raw, ang_adj, curr_ang
 
-    # ang_raw = max(-1.0, min(1.0, int((msg.pose.pose.orientation.z * 10)) / 5.0))
     ang_raw = max(-1.0, min(1.0, (msg.pose.pose.orientation.z * 2.0 * 10) / 5.0))
 
     if ang_adj is None:
         ang_adj = ang_raw
 
     # Normalize to initial value
-    target_ang = ang_raw - ang_adj
-
-    if target_ang >= ZERO_TOP:
-        if target_ang > curr_ang:
-            curr_ang = curr_ang + (INC * MULT)
-        elif target_ang < curr_ang:
-            curr_ang = curr_ang - (INC * MULT)
-        else:
-            curr_ang = target_ang
-        curr_ang = min(1.0, curr_ang)
-        # print(vels("Right   ", curr_ang, target_ang))
-    elif target_ang <= ZERO_BOTTOM:
-        if target_ang > curr_ang:
-            curr_ang = curr_ang + (INC * MULT)
-        elif target_ang < curr_ang:
-            curr_ang = curr_ang - (INC * MULT)
-        else:
-            curr_ang = target_ang
-        curr_ang = max(-1.0, curr_ang)
-        # print(vels("Left   ", curr_ang, target_ang))
-    else:
-        target_ang = 0
-        if curr_ang >= ZERO_TOP:
-            curr_ang = curr_ang - INC
-        elif curr_ang <= ZERO_BOTTOM:
-            curr_ang = curr_ang + INC
-        else:
-            curr_ang = 0
-
-            # print(vels("Angular Stop", curr_ang, target_ang))
-    curr_ang = ang_raw - ang_adj
+    curr_ang = int((ang_raw - ang_adj) * 100) / 100.0
 
 
 if __name__ == '__main__':
@@ -105,8 +43,6 @@ if __name__ == '__main__':
     ang_raw = 0
     linear_adj = None
     ang_adj = None
-    target_linear = 0
-    target_ang = 0
     curr_linear = 0
     curr_ang = 0
 
@@ -129,5 +65,5 @@ if __name__ == '__main__':
         twist.angular.y = 0
         twist.angular.z = curr_ang
         pub.publish(twist)
-        print("linear: %s\tangular: %s" % (linear_raw, ang_raw))
+        print("linear: %s\tangular: %s" % (curr_linear, curr_ang))
         rate.sleep()
