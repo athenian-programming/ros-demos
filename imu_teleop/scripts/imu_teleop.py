d354e0427c2427078d0ce290c281a99c16d9cd55
Paul Ambrose <pambrose@mac.com>
2017-09-19 21:08:14 -0700

    Misc update

--- imu_teleop/scripts/imu_teleop.py
+++ imu_teleop/scripts/imu_teleop.py
@@ -8,7 +8,7 @@ from nav_msgs.msg import Odometry
 def linear_callback(msg):
     global linear_adj, curr_linear
 
-    linear_raw = max(-1.0, min(1.0, ((msg.pose.pose.orientation.y / 2.0) * 10) / 5.0))
+    linear_raw = max(-1.0, min(1.0, ((msg.pose.pose.orientation.y / 4.0) * 10) / 5.0))
 
     if linear_adj is None:
         linear_adj = linear_raw
@@ -20,7 +20,8 @@ def linear_callback(msg):
 def ang_callback(msg):
     global ang_adj, curr_ang
 
-    ang_raw = max(-1.0, min(1.0, (msg.pose.pose.orientation.z * 2.0 * 10) / 5.0))
+    # Multiply
+    ang_raw = max(-1.0, min(1.0, (msg.pose.pose.orientation.z * 4.0 * 10) / 5.0))
 
     if ang_adj is None:
         ang_adj = ang_raw
