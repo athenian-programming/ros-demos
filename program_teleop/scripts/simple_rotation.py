cf16a9ce999ab83cd28f514742e02b7e3d93c1f3
Paul Ambrose <pambrose@mac.com>
2018-01-08 13:04:49 -0800

    Update turtle sim

--- program_teleop/scripts/simple_rotation.py
+++ program_teleop/scripts/simple_rotation.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
 
     tr = TurtleRobot(1)
 
-    print("Turning 450 degrees clockwise")
-    for a in range(0, 450, 90):
+    print("Turning 450 degrees counter-clockwise")
+    for a in range(0, 361, 90):
         tr.turn_abs(1, a)
         time.sleep(1)
 
-    print("Turning 450 degrees counter-clockwise")
-    for a in range(0, 450, 90):
+    print("Turning 450 degrees clockwise")
+    for a in range(0, 361, 90):
         tr.turn_abs(1, -a)
         time.sleep(1)
