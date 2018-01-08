efb73bd9598c1e5ec8da2e0761da7146c2a158f7
Paul Ambrose <pambrose@mac.com>
2018-01-08 13:17:00 -0800

    Update turtle sim

--- program_teleop/scripts/multi_goto.py
+++ program_teleop/scripts/multi_goto.py
@@ -6,7 +6,7 @@ from turtle_robot import TurtleRobot
 from turtle_sim import TurtleSim
 
 if __name__ == '__main__':
-    rospy.init_node('program_teleop')
+    rospy.init_node('multi_goto')
 
     TurtleSim().start()
 
@@ -15,14 +15,17 @@ if __name__ == '__main__':
     curr = tr.curr_xy
     print("Current x,y: {0},{1}".format(curr['x'], curr['y']))
 
-    print("Going to 2,2")
+    print("Going to 2, 2")
     tr.goto(2, 2, .25)
 
-    print("Going to 2,9")
+    print("Going to 2, 9")
     tr.goto(2, 9, .25)
 
-    print("Going to 9,9")
+    print("Going to 9, 9")
     tr.goto(9, 9, .25)
 
-    print("Going to 9,2")
+    print("Going to 9, 2")
     tr.goto(9, 2, .25)
+
+    print("Going to 5.5, 5.5")
+    tr.goto(5.5, 5.5, .25)
--- program_teleop/scripts/simple_rotation.py
+++ program_teleop/scripts/simple_rotation.py
@@ -8,13 +8,12 @@ from turtle_robot import TurtleRobot
 from turtle_sim import TurtleSim
 
 if __name__ == '__main__':
-    rospy.init_node('program_teleop')
+    rospy.init_node('simple_rotation')
 
     TurtleSim().start()
-
     tr = TurtleRobot(1)
 
-    print("Turning 369 degrees counter-clockwise")
+    print("Turning 360 degrees counter-clockwise")
     for a in range(0, 361, 90):
         tr.turn_abs(1, a)
         time.sleep(1)
