c587bab24c33d390247c8b0e303538fc7df6ecc1
Paul Ambrose <pambrose@mac.com>
2018-01-09 12:49:38 -0800

    Update turtle sim

--- program_teleop/scripts/raw_twist_with_rate.py
+++ program_teleop/scripts/raw_twist_with_rate.py
@@ -3,6 +3,8 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+from turtle_robot import TurtleRobot
+
 if __name__ == '__main__':
 
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
@@ -10,14 +12,7 @@ if __name__ == '__main__':
     rate = rospy.Rate(10)
 
     for i in range(5):
-        t = Twist()
-        t.linear.x = .5
-        t.linear.y = 0
-        t.linear.z = 0
-        t.angular.x = 0
-        t.angular.y = 0
-        t.angular.z = 0
-
-        pub.publish(t)
-
+        pub.publish(TurtleRobot.new_twist(0, .5))
         rate.sleep()
+
+    pub.publish(TurtleRobot.new_twist(0, 0))
