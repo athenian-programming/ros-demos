4cfdbf118475310aa175c5b483848c5d0c7180e9
Paul Ambrose <pambrose@mac.com>
2018-01-09 08:34:44 -0800

    Add raw versions

---
+++ program_teleop/scripts/raw_twist_no_rate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import time
+
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == '__main__':
+
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+
+    for i in range(5):
+        t = Twist()
+        t.linear.x = .5
+        t.linear.y = 0
+        t.linear.z = 0
+        t.angular.x = 0
+        t.angular.y = 0
+        t.angular.z = 0
+
+        pub.publish(t)
+
+        time.sleep(.1)
